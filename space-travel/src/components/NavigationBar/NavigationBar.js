10c2d2f3b9c7c2b741d47dac8af26577b831452d
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-11-28 22:17:30 -0800

    Add files via upload

---
+++ space-travel/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,30 @@
+import {NavLink} from "react-router-dom";
+
+import styles from "./NavigationBar.module.css";
+
+function NavigationBar ()
+{
+  const routes = [
+    {to: "/", text: "🌎 Trap House"},
+    {to: "/spacecrafts", text: "💪🚀 Spacecrafts"},
+    {to: "/planets", text: "🪐👌 Planets"}
+  ];
+
+  return (
+    <nav className={styles["navigation"]}>
+      {
+        routes.map((route, index) =>
+                     <NavLink
+                       key={index}
+                       className={({isActive, isPending}) => `${styles["navigation__item"]} ${isActive ? styles["navigation__item--active"] : ""}`}
+                       to={route.to}
+                     >
+                       {route.text}
+                     </NavLink >
+        )
+      }
+    </nav>
+  );
+}
+
+export default NavigationBar;
---
+++ space-travel/src/components/Obamium/Obamium.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Obamium(){
+     return (
+            <img src="https://static.wikia.nocookie.net/peuntas/images/1/11/Obamium.jpg/revision/latest?cb=20200616152202" alt="React Image" width={200} height={200}/>
+     )
+}
+  
